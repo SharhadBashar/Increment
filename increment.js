e4892c2f7214ba3060d26db255f68dd48b123461
Sharhad Bashar <SharhadBashar@users.noreply.github.com>
2019-07-29 20:00:34 -0400

    Update increment.js

--- increment.js
+++ increment.js
@@ -1,1 +1,19 @@
+function increment (num) {
+    for (var i = num.length - 1; i >= 0; i--) {
+        num[i]++;
+        if (num[i] > 9) {
+          num[i] = 0;
+          if (i === 0) {
+            // Last iteration, but we need to carry - unshift a 1:
+            num.unshift(1);
+          }
+        }
+        else break;
+    }
+    return num;
+}
 
+increment([1, 2, 7, 9]); //returns [1, 2, 8, 0]
+increment([3, 9, 9, 9]); //returns [4, 0, 0, 0]
+increment([9, 9, 9, 9]); //returns [1, 0, 0, 0, 0]
+increment([1, 1, 1, 1]); //returns [1, 1, 1, 2]
